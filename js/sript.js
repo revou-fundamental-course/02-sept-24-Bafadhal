c8dc35b1855162003052e2b675b18762a0401784
Bafadhal <alamsyahbafadhal@gmail.com>
2024-09-09 23:43:11 +0700

    update

--- js/sript.js
+++ js/sript.js
@@ -1,1 +1,16 @@
-// Ini Javasript
+function hitungLuas() {
+    let alas = parseFloat(document.getElementById("alas").value);
+    let tinggi = parseFloat(document.getElementById("tinggi").value);
+
+    if (isNaN(alas) || isNaN(tinggi)) {
+        document.getElementById("result").innerText = "Masukkan nilai alas dan tinggi yang valid!";
+        return;
+    }
+
+    let result = 0.5 * alas * tinggi;
+
+    document.getElementById("result").innerText = 
+        `L = 1/2 x a x t\n` +
+        `L = 1/2 x ${alas} x ${tinggi}\n` +
+        `L = ${result}`;
+}
