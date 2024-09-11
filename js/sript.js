17e97155bf00898c2fac468793d814bdfa61ff72
Bafadhal <alamsyahbafadhal@gmail.com>
2024-09-11 20:11:15 +0700

    update

--- js/sript.js
+++ js/sript.js
@@ -3,7 +3,7 @@ function hitungLuas() {
     let tinggi = parseFloat(document.getElementById("tinggi").value);
 
     if (isNaN(alas) || isNaN(tinggi)) {
-        document.getElementById("result").innerText = "Masukkan nilai alas dan tinggi yang valid!";
+        document.getElementById("result").innerText = "Form masih kosong!";
         return;
     }
 
@@ -16,21 +16,21 @@ function hitungLuas() {
 }
 
 function hitungKeliling() {
-    // Mengambil nilai sisi-sisi segitiga
+
     let s1 = parseFloat(document.getElementById("sisiA").value);
     let s2 = parseFloat(document.getElementById("sisiB").value);
     let s3 = parseFloat(document.getElementById("sisiC").value);
 
-    // Mengecek jika input tidak kosong atau bukan angka
+
     if (isNaN(s1) || isNaN(s2) || isNaN(s3)) {
-        document.getElementById("keliling-result").innerText = "Masukkan nilai sisi yang valid!";
+        document.getElementById("keliling-result").innerText = "Form masih kosong!";
         return;
     }
 
-    // Menghitung keliling segitiga
+
     let result = s1 + s2 + s3;
 
-    // Menampilkan hasil perhitungan yang tersusun ke bawah
+
     document.getElementById("keliling-result").innerText = 
         `K = S1 + S2 + S3\n` +
         `K = ${s1} + ${s2} + ${s3}\n` +
